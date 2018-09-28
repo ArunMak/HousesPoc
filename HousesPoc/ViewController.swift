4ea76e9a9dd62c1606d1921241c398694a8491f5
Parthiban <parthisivaraj@gmail.com>
2018-09-29 00:14:36 +0530

    Initial Commit

---
+++ HousesPoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HousesPoc
+//
+//  Created by ArunMak on 29/09/18.
+//  Copyright © 2018 ArunMak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
